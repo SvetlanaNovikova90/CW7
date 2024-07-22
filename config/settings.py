4e9bf5595c0fbaa94af4c5c3bae5cb7c1d499ba4
Svetlana Novikova <seaciliya2012@yandex.ru>
2024-07-22 16:26:08 +0300

    "cw7 2"

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
 
+    'users',
+    'habit',
+
 ]
 
 MIDDLEWARE = [
@@ -115,3 +118,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+AUTH_USER_MODEL = "users.User"
